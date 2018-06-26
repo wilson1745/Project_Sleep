f7031a1fce0d46f415736713dfd9796b344885dd
wilson1745 <wilson-power0130@hotmail.com>
2018-06-26 13:05:02 +0100

    LightTest lux test

---
+++ app/src/main/java/e/wilso/project_sleep/LightTestActivity.java
@@ -0,0 +1,13 @@
+package e.wilso.project_sleep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LightTestActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_light_test);
+   }
+}
