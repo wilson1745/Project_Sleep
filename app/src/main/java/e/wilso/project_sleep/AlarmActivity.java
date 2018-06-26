f1de46adce8e219937311cd7cee665d9a136b97b
wilson1745 <wilson-power0130@hotmail.com>
2018-06-26 12:16:47 +0100

    AlarmActivity check

--- app/src/main/java/e/wilso/project_sleep/AlarmActivity.java
+++ app/src/main/java/e/wilso/project_sleep/AlarmActivity.java
@@ -2,6 +2,7 @@ package e.wilso.project_sleep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AlarmActivity extends AppCompatActivity {
 
@@ -9,5 +10,30 @@ public class AlarmActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_alarm);
+
+      getSupportActionBar().setTitle("Alarm Setting");
+      setBackbutton();
+   }
+
+   private void setBackbutton() {
+      if(getSupportActionBar() != null) {
+         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+         getSupportActionBar().setDisplayShowHomeEnabled(true);
+      }
+   }
+
+   @Override
+   public void onBackPressed() {
+      int count = getFragmentManager().getBackStackEntryCount();
+
+      if (count == 0) super.onBackPressed();
+      else getFragmentManager().popBackStack();
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      if(item.getItemId() == android.R.id.home) finish();
+
+      return super.onOptionsItemSelected(item);
    }
 }
