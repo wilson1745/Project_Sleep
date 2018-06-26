2212f69eb372408260f2fea7901a12896f161fc8
wilson1745 <wilson-power0130@hotmail.com>
2018-06-26 12:06:04 +0100

    AlarmActivity

--- app/src/main/java/e/wilso/project_sleep/menu/HomeFragment.java
+++ app/src/main/java/e/wilso/project_sleep/menu/HomeFragment.java
@@ -1,12 +1,15 @@
 package e.wilso.project_sleep.menu;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import e.wilso.project_sleep.AlarmActivity;
 import e.wilso.project_sleep.MainActivity;
 import e.wilso.project_sleep.R;
 
@@ -15,19 +18,31 @@ import e.wilso.project_sleep.R;
  */
 public class HomeFragment extends Fragment {
 
+   private Button btn_alarm;
+   private View view;
+   private Intent intent;
 
    public HomeFragment() {
       // Required empty public constructor
    }
 
-
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
       ((MainActivity)getActivity()).setActionBartTitle("Home");
+      view = inflater.inflate(R.layout.fragment_home, container, false);
+
+      btn_alarm = view.findViewById(R.id.btn_alarm);
+      btn_alarm.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            intent = new Intent(getActivity(), AlarmActivity.class);
+            startActivity(intent);
+            Toast.makeText(getActivity(), "AlarmActivity", Toast.LENGTH_SHORT).show();
+         }
+      });
 
       // Inflate the layout for this fragment
-      return inflater.inflate(R.layout.fragment_home, container, false);
+      return view;
    }
 
 }
