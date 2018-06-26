8297b70fe769e019830593d2b1016f2388b35c6a
wilson1745 <wilson-power0130@hotmail.com>
2018-06-26 13:16:31 +0100

    LightTest lux test check

--- app/src/main/java/e/wilso/project_sleep/LightTestActivity.java
+++ app/src/main/java/e/wilso/project_sleep/LightTestActivity.java
@@ -2,6 +2,7 @@ package e.wilso.project_sleep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LightTestActivity extends AppCompatActivity {
 
@@ -9,5 +10,32 @@ public class LightTestActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_light_test);
+
+      getSupportActionBar().setTitle("Lux Test");
+      setBackbutton();
+
+
+   }
+
+   private void setBackbutton() {
+      if (getSupportActionBar() != null) {
+         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+         getSupportActionBar().setDisplayShowHomeEnabled(true);
+      }
+   }
+
+   @Override
+   public void onBackPressed() {
+      int count = getFragmentManager().getBackStackEntryCount();
+
+      if (count == 0) super.onBackPressed();
+      else getFragmentManager().popBackStack();
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      if (item.getItemId() == android.R.id.home) finish();
+
+      return super.onOptionsItemSelected(item);
    }
 }
--- app/src/main/java/e/wilso/project_sleep/menu/HomeFragment.java
+++ app/src/main/java/e/wilso/project_sleep/menu/HomeFragment.java
@@ -10,15 +10,16 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import e.wilso.project_sleep.AlarmActivity;
+import e.wilso.project_sleep.LightTestActivity;
 import e.wilso.project_sleep.MainActivity;
 import e.wilso.project_sleep.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
-   private Button btn_alarm;
+   private Button btn_alarm, btn_light;
    private View view;
    private Intent intent;
 
@@ -29,20 +30,35 @@ public class HomeFragment extends Fragment {
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       ((MainActivity)getActivity()).setActionBartTitle("Home");
-      view = inflater.inflate(R.layout.fragment_home, container, false);
+      findView(inflater, container);
 
-      btn_alarm = view.findViewById(R.id.btn_alarm);
-      btn_alarm.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-            intent = new Intent(getActivity(), AlarmActivity.class);
-            startActivity(intent);
-            Toast.makeText(getActivity(), "AlarmActivity", Toast.LENGTH_SHORT).show();
-         }
-      });
+      btn_alarm.setOnClickListener(this);
+      btn_light.setOnClickListener(this);
 
       // Inflate the layout for this fragment
       return view;
    }
 
+   private void findView(LayoutInflater inflater, ViewGroup container) {
+      view = inflater.inflate(R.layout.fragment_home, container, false);
+      btn_alarm = view.findViewById(R.id.btn_alarm);
+      btn_light = view.findViewById(R.id.btn_light);
+
+   }
+
+   @Override
+   public void onClick(View v) {
+      int i = v.getId();
+
+      if(i == btn_alarm.getId()) {
+         intent = new Intent(getActivity(), AlarmActivity.class);
+         startActivity(intent);
+         Toast.makeText(getActivity(), "AlarmActivity", Toast.LENGTH_SHORT).show();
+      }
+      else if(i == btn_light.getId()) {
+         intent = new Intent(getActivity(), LightTestActivity.class);
+         startActivity(intent);
+         Toast.makeText(getActivity(), "LightTestActivity", Toast.LENGTH_SHORT).show();
+      }
+   }
 }
